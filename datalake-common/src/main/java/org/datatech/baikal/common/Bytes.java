8863222b1a4b1b4d5ff72eadb817bdbc893de9e6
jinkei98 <jinjj@paraview.cn>
2018-12-13 15:30:36 +0800

    Refactor 'Byte' class to common modules and update comments

--- datalake-common/src/main/java/org/datatech/baikal/common/Bytes.java
+++ datalake-common/src/main/java/org/datatech/baikal/common/Bytes.java
@@ -12,58 +12,24 @@
  * limitations under the License.
  */
 
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.datatech.baikal.task.util;
+package org.datatech.baikal.common;
 
 import java.nio.charset.Charset;
 
 /**
- * Bytes class for byte array operation.
+ * Byte array related functions.
  */
 public class Bytes {
     private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
-    /**
-     * Convert string to byte array.
-     *
-     * @param s String.
-     * @return Byte array.
-     */
     public static byte[] toBytes(String s) {
         return s.getBytes(UTF8_CHARSET);
     }
 
-    /**
-     * Convert byte array to string.
-     *
-     * @param b Byte array.
-     * @return String.
-     */
     public static String toString(byte[] b) {
         return b == null ? null : toString(b, 0, b.length);
     }
 
-    /**
-     * Convert byte array to string from certain offset and length.
-     *
-     * @param b   Byte array.
-     * @param off Start from the <b>offset</b> from the byte array.
-     * @param len Length.
-     * @return String.
-     */
     public static String toString(byte[] b, int off, int len) {
         if (b == null) {
             return null;
@@ -72,12 +38,6 @@ public class Bytes {
         }
     }
 
-    /**
-     * Convert integer to byte array with 4 byte length.
-     *
-     * @param val Integer value.
-     * @return Byte array.
-     */
     public static byte[] toBytes(int val) {
         byte[] b = new byte[4];
 
